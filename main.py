a9fb4d5b4d942b0120abf99470f6090b63ce5f05
Sosso Brain <Baudrymbome@gmail.com>
2023-09-26 04:44:43 -0400

    yo

--- main.py
+++ main.py
@@ -1,33 +1,28 @@
 import os
-import smtplib
-from dotenv import load_dotenv
-from utils import set_environment_for_proxy, extract_smtp_details_from_sample, send_test_email
+from dotenv import load_dotenv, find_dotenv
+from utils import extract_smtp_details_from_sample, send_test_email
 
 # Load environment variables
-load_dotenv()
+load_dotenv(find_dotenv())
 
 # File Names
-SMTP_SAMPLE_FILE                    = "smtps.txt"
-WORKING_SMTP_FILE                   = "working_smtps.txt"
-FAILED_SMTP_FILE                    = "failed_smtps.txt"
+SMTP_SAMPLE_FILE                = "smtps.txt"
+WORKING_SMTP_FILE               = "working_smtps.txt"
+FAILED_SMTP_FILE                = "failed_smtps.txt"
 
 def main():
     """
     Main function to execute the SMTP testing process.
     """
     try:
-        # Set environment for proxy or SOCKS (if needed)
-        set_environment_for_proxy()
-
         # SMTP Testing
-        smtp_details_list           = extract_smtp_details_from_sample(SMTP_SAMPLE_FILE)
+        smtp_details_list       = extract_smtp_details_from_sample(SMTP_SAMPLE_FILE)
 
         for smtp_details in smtp_details_list:
+            success, message    = send_test_email(smtp_details)
+            smtp_format         = f"URL: {smtp_details.get('URL', 'N/A')}\nMETHOD: {smtp_details.get('METHOD', 'N/A')}\nMAILHOST: {smtp_details['MAILHOST']}\nMAILPORT: {smtp_details['MAILPORT']}\nMAILUSER: {smtp_details['MAILUSER']}\nMAILPASS: {smtp_details['MAILPASS']}\nMAILFROM: {smtp_details.get('MAILFROM', 'N/A')}\nFROMNAME: {smtp_details.get('FROMNAME', 'N/A')}\n\n"
 
-            success, message        = send_test_email(smtp_details)
-            smtp_format             = f"URL: {smtp_details['URL']}\nMETHOD: {smtp_details['METHOD']}\nMAILHOST: {smtp_details['MAILHOST']}\nMAILPORT: {smtp_details['MAILPORT']}\nMAILUSER: {smtp_details['MAILUSER']}\nMAILPASS: {smtp_details['MAILPASS']}\nMAILFROM: {smtp_details['MAILFROM']}\nFROMNAME: {smtp_details['FROMNAME']}\n\n"
             with open(WORKING_SMTP_FILE if success else FAILED_SMTP_FILE, 'a') as file:
-
                 file.write(smtp_format)
                 if not success:
                     file.write(f"ERROR: {message}\n")
--- utils.py
+++ utils.py
@@ -1,62 +1,80 @@
 import os
+import random
 import smtplib
+import socks
 
 # ==================
-# Set Environment for Proxy
+# Load Environment Variable
 # ==================
-def set_environment_for_proxy():
+def load_env_variables():
     """
-    Set the HTTP_PROXY and HTTPS_PROXY environment variables if proxies or SOCKS are to be used.
+    Load necessary environment variables.
+    Returns: Dictionary containing SMTP details.
     """
-    try:
-        if os.getenv("USE_PROXIES_HTTP", "0") == "1":
-            with open("http_proxies.txt", "r") as file:
-                proxy = file.readline().strip()
-                os.environ["HTTP_PROXY"] = f"http://{proxy}"
-                os.environ["HTTPS_PROXY"] = f"http://{proxy}"
-        elif os.getenv("USE_PROXIES_SOCKS", "0") == "1":
-            with open("socks.txt", "r") as file:
-                sock = file.readline().strip()
-                os.environ["HTTP_PROXY"] = f"socks5://{sock}"
-                os.environ["HTTPS_PROXY"] = f"socks5://{sock}"
-    except Exception as e:
-        print(f"Error setting up proxy/socks environment: {e}")
+    return {
+        "EMAIL_SUBJECT": os.getenv("EMAIL_SUBJECT", "Test Email from SMTP Checker"),
+        "EMAIL_BODY": os.getenv("EMAIL_BODY", "This is a test email sent by the SMTP Checker tool."),
+        "RECIPIENT": os.getenv("RECIPIENT", "test@example.com"),
+        "USE_PROXIES_HTTP": os.getenv("USE_PROXIES_HTTP", "0"),
+        "USE_PROXIES_SOCKS": os.getenv("USE_PROXIES_SOCKS", "0")
+    }
 
 # ==================
 # Extract SMTP Details
 # ==================
-def extract_smtp_details_from_sample(filename):
+def extract_smtp_details_from_sample(line):
     """
-    Extract SMTP details from the sample file.
-
+    Extract SMTP details from a given line of the sample file.
     Args:
-    - filename (str): The name of the sample file.
-
+    - line (str): Line from SMTP sample file.
     Returns:
-    - list: List of dictionaries containing SMTP details.
+    - Dictionary containing SMTP details or None if the line format is unexpected.
     """
     try:
-        with open(filename, "r") as file:
-            lines = file.readlines()
-
-        smtp_details_list = []
-        smtp_details = {}
-        for line in lines:
-            if line.strip() == "":
-                if smtp_details:
-                    smtp_details_list.append(smtp_details)
-                    smtp_details = {}
-            else:
-                key, value = line.split(": ", 1)
-                smtp_details[key] = value.strip()
-        if smtp_details:  # For the last set of details
-            smtp_details_list.append(smtp_details)
-
-        return smtp_details_list
-
+        details         = line.strip().split(":")
+        if len(details) != 4:
+            return None
+        return {
+            "MAILUSER": details[0],
+            "MAILPASS": details[1],
+            "MAILHOST": details[2],
+            "MAILPORT": details[3]
+        }
     except Exception as e:
-        print(f"Error extracting SMTP details from {filename}: {e}")
-        return []
+        print(f"Error extracting SMTP details from {line}: {e}")
+        return None
+
+# ==================
+# Retrieve Random Proxy
+# ==================
+def get_random_proxy(proxy_type):
+    """
+    Fetch a random proxy from the respective proxy file.
+    Args:
+    - proxy_type (str): Type of proxy ("http" or "socks").
+    Returns:
+    - str: Proxy in format ip:port or None if an error occurred.
+    """
+    try:
+        if proxy_type == "http":
+            filename        = "http_proxies.txt"
+
+        elif proxy_type == "socks":
+            filename        = "socks.txt"
+
+        else:
+            return None
+
+        with open(filename, 'r') as f:
+            proxies         = f.readlines()
+
+        if not proxies:
+            return None
+
+        return random.choice(proxies).strip()
+    except Exception as e:
+        print(f"Error fetching random {proxy_type} proxy: {e}")
+        return None
 
 # ==================
 # Send Test Email
@@ -64,30 +82,46 @@ def extract_smtp_details_from_sample(filename):
 def send_test_email(smtp_details):
     """
     Send a test email using the provided SMTP details.
-
     Args:
     - smtp_details (dict): Dictionary containing SMTP details.
-
     Returns:
     - bool: True if the email was sent successfully, False otherwise.
     - str: Message indicating the result or the error.
     """
     try:
         # Extracting SMTP details
-        SMTP_USER = smtp_details["MAILUSER"]
-        SMTP_PASS = smtp_details["MAILPASS"]
-        SMTP_HOST = smtp_details["MAILHOST"]
-        SMTP_PORT = int(smtp_details["MAILPORT"])
+        SMTP_USER               = smtp_details["MAILUSER"]
+        SMTP_PASS               = smtp_details["MAILPASS"]
+        SMTP_HOST               = smtp_details["MAILHOST"]
+        SMTP_PORT               = int(smtp_details["MAILPORT"])
+
+        # Check if using HTTP proxy
+        if os.getenv("USE_PROXIES_HTTP", "0") == "1":
+            proxy               = get_random_proxy("http")
+            if proxy:
+                ip, port        = proxy.split(":")
+                socket.setdefaulttimeout(60)
+                socket.socket   = socks.socksocket
+                socks.setdefaultproxy(socks.PROXY_TYPE_HTTP, ip, int(port))
+
+        # Check if using SOCKS proxy
+        if os.getenv("USE_PROXIES_SOCKS", "0") == "1":
+            proxy               = get_random_proxy("socks")
+
+            if proxy:
+                ip, port        = proxy.split(":")
+                socks.set_default_proxy(socks.SOCKS5, ip, int(port))
+                socks.wrap_module(smtplib)
 
         # Email content from environment variables
-        EMAIL_SUBJECT = os.getenv("EMAIL_SUBJECT", "Test Email from SMTP Checker")
-        EMAIL_BODY = os.getenv("EMAIL_BODY", "This is a test email sent by the SMTP Checker tool.")
-        RECIPIENT = os.getenv("RECIPIENT", "test@example.com")
+        EMAIL_SUBJECT           = os.getenv("EMAIL_SUBJECT", "Test Email from SMTP Checker")
+        EMAIL_BODY              = os.getenv("EMAIL_BODY", "This is a test email sent by the SMTP Checker tool.")
+        RECIPIENT               = os.getenv("RECIPIENT", "test@example.com")
 
         if SMTP_PORT == 465:
-            server = smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT)
+            server              = smtplib.SMTP_SSL(SMTP_HOST, SMTP_PORT)
         elif SMTP_PORT == 587:
-            server = smtplib.SMTP(SMTP_HOST, SMTP_PORT)
+            server              = smtplib.SMTP(SMTP_HOST, SMTP_PORT)
             server.starttls()
         else:
             raise ValueError(f"Unsupported SMTP port: {SMTP_PORT}")
@@ -99,3 +133,4 @@ def send_test_email(smtp_details):
 
     except Exception as e:
         return False, str(e)
+
